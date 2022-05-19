3f07bd0bdbf933cdde192a0478a7de7c60176f7a
MiaaaV <sirivesa@gmail.com>
2022-05-19 12:53:17 +0300

    form validation working

--- pages/dogs.js
+++ pages/dogs.js
@@ -1,51 +1,22 @@
-/* FUNCTION FOR CHECKING REQUIRED FIELDS */
-
-
-/* FUNCTION FOR REST */
-
 let textBox = document.getElementById("cta-text-box");
 let container = document.getElementById("rest-container");
+/* FUNCTION FOR CHECKING REQUIRED FIELDS */
 
-
-function registerNow() {
-
-    document.getElementById("name-field").innerHTML = "";
-    document.getElementById("email-field").innerHTML = "";
-    document.getElementById("password-field").innerHTML = "";
-
-    const NAME_ERROR = "Name cannot include numbers.";
-    const EMAIL_ERROR = "Email must include @ -sign.";
-    const PASSW_ERROR = "Password must be at least 8 characters long.";
-
-    let name = document.getElementById("full-name").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-
-
-    if (name !== 1) {
-        document.getElementById("name-field").innerHTML = NAME_ERROR;
+function validateForm() {
+    let x = document.forms["myForm"]["full-name"].value;
+    if (x == "") {
+        return false;
     }
-    if (email !== "@") {
-        document.getElementById("email-field").innerHTML = EMAIL_ERROR;
-
-    }
-    if (password.length !== 8) {
-        document.getElementById("password-field").innerHTML = PASSW_ERROR;
-
-    }
-
 }
 
+let form = document.getElementById('myForm');
 
-function getDog() {
+form.onsubmit = function(e) {
 
-    textBox.addEventListener("click", changeDisplay)
-    container.addEventListener("click", changeDisplay)
+    e.preventDefault();
 
-    function changeDisplay() {
-        textBox.classList.toggle("active");
-        container.classList.toggle("active");
-    }
+    //fetch code here
+    changeDisplay();
 
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
@@ -56,17 +27,18 @@ function getDog() {
             }
         )
 
+
+}
+
+
+/* FUNCTION FOR REST */
+
+function changeDisplay() {
+    textBox.classList.toggle("active");
+    container.classList.toggle("active");
 }
 
 function moreDogs() {
-
-    textBox.addEventListener("click", changeDisplay)
-    container.addEventListener("click", changeDisplay)
-
-    function changeDisplay() {
-        textBox.classList.add("active");
-        container.classList.remove("active");
-    }
 
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
